93c04efd596b5e655080cf66a36336172f42a8c4
Felipemendezrios <famendezr@unal.edu.co>
2025-08-01 17:25:45 +0200

    Likelihood function

--- Development/run_model_BaM/Functions/Structural_error_models.R
+++ Development/run_model_BaM/Functions/Structural_error_models.R
@@ -1,11 +1,24 @@
-llfunk_Felipe <- function(Ysim, Yobs, Yu, gamma, x) {
+#' Structural error model
+#'
+#' Linear depending on space covariate for WSE and Gaussian in discharge and velocity
+#'
+#' @param Ysim numeric vector, simulated values
+#' @param Yobs numeric vector, observed values
+#' @param Yu numeric vector, uncertainty in observed values
+#' @param gamma data frame, parameters of structural error model
+#' @param x numeric vector, space covariate
+#'
+#' @return real value, loglikelihood computation
+#'
+#' @export
+llfunk_Linear_X_in_Z_Gaussian_QV <- function(Ysim, Yobs, Yu, gamma, x) {
     p <- NCOL(Ysim)
     out <- 0
     for (i in 1:p) {
         if (i == 1) {
             g0 <- gamma[2 * i - 1]
             g1 <- gamma[2 * i]
-            s <- g0 + g1 * abs(x)
+            s <- abs(g0) + abs(g1) * abs(x)
         } else {
             s <- gamma[1 + i] # Case Q and V
         }
