84921c17e32c6651cd6448b42acdaf531249a7a9
Felipemendezrios <famendezr@unal.edu.co>
2025-08-05 09:57:36 +0200

    Create a check LogPrior verification given the prior information as input (by using RBaM::parameter) and calculate the LogPrior automatically. However, a LogPrior function is must defined each time, because the same vector is shared for LogLikelihood fonction.

--- Development/run_model_BaM/Functions/Estimation.R
+++ Development/run_model_BaM/Functions/Estimation.R
@@ -1,54 +1,51 @@
-logPrior <- function(prior_param_case) {
-    n_type_param <- length(prior_param_case)
+logPrior_Verification <- function(prior_param_case) {
     out <- 0
-    for (j in 1:n_type_param) {
-        nb_param <- length(prior_param_case[[j]][[1]])
-        for (i in 1:nb_param) {
-            initial_guess <- prior_param_case[[j]][[1]]
-            distribution <- prior_param_case[[j]][[2]]
-            distribution_param <- prior_param_case[[j]][[3]]
+    nb_param <- length(prior_param_case)
+    for (i in 1:nb_param) {
+        initial_guess <- prior_param_case[[i]]$init
+        distribution <- prior_param_case[[i]]$prior$dist
+        distribution_param <- prior_param_case[[i]]$prior$par
 
-            out_temp <-
-                if (distribution[[i]] == "FIX") {
-                    # Default values: only for not avoiding to accept all samples
-                    dlnorm(initial_guess[[i]],
-                        meanlog = log(initial_guess[[i]]),
-                        sdlog = 0.2,
-                        log = TRUE
-                    )
-                } else if (
-                    distribution[[i]] == "FlatPrior" | distribution[[i]] == "FlatPrior+" | distribution[[i]] == "FlatPrior-"
-                ) {
-                    0
-                } else if (
-                    distribution[[i]] == "Gaussian"
-                ) {
-                    dnorm(initial_guess[[i]],
-                        mean = distribution_param[[i]][1],
-                        sd = distribution_param[[i]][2],
-                        log = TRUE
-                    )
-                } else if (
-                    distribution[[i]] == "Uniform"
-                ) {
-                    dunif(initial_guess[[i]],
-                        min = distribution_param[[i]][1],
-                        max = distribution_param[[i]][2],
-                        log = TRUE
-                    )
-                } else if (
-                    distribution[[i]] == "LogNormal"
-                ) {
-                    dlnorm(initial_guess[[i]],
-                        meanlog = distribution_param[[i]][1],
-                        sdlog = distribution_param[[i]][2],
-                        log = TRUE
-                    )
-                } else {
-                    stop(paste0("The Prior distribution: ", distribution[[i]], " is not supported"))
-                }
-            out <- out + out_temp
-        }
+        out_temp <-
+            if (distribution == "FIX") {
+                # Default values: only for not avoiding to accept all samples
+                dlnorm(initial_guess,
+                    meanlog = log(initial_guess),
+                    sdlog = 0.2,
+                    log = TRUE
+                )
+            } else if (
+                distribution == "FlatPrior" | distribution == "FlatPrior+" | distribution == "FlatPrior-"
+            ) {
+                0
+            } else if (
+                distribution == "Gaussian"
+            ) {
+                dnorm(initial_guess,
+                    mean = distribution_param[1],
+                    sd = distribution_param[2],
+                    log = TRUE
+                )
+            } else if (
+                distribution == "Uniform"
+            ) {
+                dunif(initial_guess,
+                    min = distribution_param[1],
+                    max = distribution_param[2],
+                    log = TRUE
+                )
+            } else if (
+                distribution == "LogNormal"
+            ) {
+                dlnorm(initial_guess,
+                    meanlog = distribution_param[1],
+                    sdlog = distribution_param[2],
+                    log = TRUE
+                )
+            } else {
+                stop(paste0("The Prior distribution: ", distribution, " is not supported"))
+            }
+        out <- out + out_temp
     }
     if (is.na(out) | is.infinite(out)) {
         out <- -Inf
