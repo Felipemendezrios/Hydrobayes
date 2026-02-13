467fbc157b08219adfcb0ebf487f561fcaa32e83
Felipemendezrios <famendezr@unal.edu.co>
2026-02-13 16:38:53 +0100

    rename file to setting.r

--- R/config_paths.r
+++
@@ -1,2 +0,0 @@
-dir_exe_BaM <- "/home/famendezrios/Documents/Git/BaM/makefile/"
-MAGE_executable <- "/home/famendezrios/Documents/Softwares/pamhyr2/mage8/mage"
---
+++ R/settings.r
@@ -0,0 +1,8 @@
+dir_exe_BaM <- "/home/famendezrios/Documents/Git/BaM/makefile/"
+MAGE_executable <- "/home/famendezrios/Documents/Softwares/pamhyr2/mage8/mage"
+
+
+# Setting jump standard deviation for MCMC sampling
+jump_MCMC_theta_param_user <- 8
+jump_MCMC_error_model_user <- 0.001
+threshold_jump_MCMC_error_model <- 0.5
