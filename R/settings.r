5d8ff229cf48bee3b98c72e4bdbf8284c06ce16b
Felipemendezrios <famendezr@unal.edu.co>
2026-02-18 17:15:25 +0100

    Modify the exe of mage : now it comes from the git repository

--- R/settings.r
+++ R/settings.r
@@ -1,5 +1,6 @@
 dir_exe_BaM <- "/home/famendezrios/Documents/Git/BaM/makefile/"
-MAGE_executable <- "/home/famendezrios/Documents/Softwares/pamhyr2/mage8/mage"
+# MAGE_executable <- "/home/famendezrios/Documents/Softwares/pamhyr2/mage8/mage"
+MAGE_executable <- "/home/famendezrios/Documents/Git/mage/exe/mage"
 
 
 # Setting jump standard deviation for MCMC sampling
