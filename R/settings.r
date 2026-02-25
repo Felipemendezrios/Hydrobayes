3819878ad4ea88a466578978ac25f9f12a2cf46c
Felipemendezrios <famendezr@unal.edu.co>
2026-02-25 18:22:51 +0100

    Remove case specificities from setting, because this module is transversal of study case

--- R/settings.r
+++ R/settings.r
@@ -1,16 +1,6 @@
 MAGE_executable <- "/home/famendezrios/Documents/Git/mage/exe/mage"
 
-# Setting jump standard deviation for MCMC sampling
-jump_MCMC_theta_param_user <- 8
-jump_MCMC_error_model_user <- 0.001
-threshold_jump_MCMC_error_model <- 0.5
-
 # Calibration
 ID_model_BaM <- "MAGE_ZQV"
 nX_BaM <- 4
 nY_BaM <- 5
-
-# Logical
-do_calibration <- FALSE
-do_plot_calibration <- TRUE
-do_prediction <- TRUE
