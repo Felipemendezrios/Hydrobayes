a4f9be64b1037305c26faf8724c57e1681f4db11
Felipemendezrios <famendezr@unal.edu.co>
2026-03-18 08:10:43 +0100

    Main changes: - Change name of XR to Typology (idea to tributary or main channel) - Add information of reaches of HM to compare if boundaries defined by all SU of a typology cover all KP of the HM - Decision (reference): keep left value and exclude right except for last interval (boundaries) - Add id_reach_SU_boundaries: id representing the limits of each SU. This is a KP vector with two values: start and end of the SU domain. Here two case are implemented following the covariate function. * Case Legendre polynomial: only the limits values are considered (SU domain) * Case piecewise function: subdivision in a SU is allowed, so boundaries are composed by limits and intermedial cuts. It is assigned following the KP discretized for numerical reasons instead of the SU input information. However there are strongly related, one comes from the another. - Important: new SU propertie -> id_reach_SU assigned at each KP for Kmin and Kflood separately. It will help to create the groups for plotting - Add summary_SU for for all typologies for Kmin and Kflood.

--- R/constructor_spatial_unit.r
+++ R/constructor_spatial_unit.r
@@ -28,8 +28,9 @@ RUGFile_constructor_SU_correction <- function(Key_Info_XR_MR) {
 }
 
 
-SU_constructor <- function(SU_key_HM,
-                           Key_Info_Typology_Model_Reach) {
+SU_constructor <- function(
+    SU_key_HM,
+    Key_Info_Typology_Model_Reach) {
     # Initialize structure by fixing layer 1 : XR
     # Layer 2 : Kmin or Kmoy is handle outside this function
     SU <- vector(mode = "list", length = length(SU_key_HM))
@@ -42,12 +43,19 @@ SU_constructor <- function(SU_key_HM,
         })
     })
 
-    # Loop through XR
-    for (id_XR in seq_along(SU_key_HM)) {
-        # Get KP boundaries of all SU at a XR
-        SU_KP_boundaries_list <- SU_KP_boundaries_structure[[id_XR]]
-        RUG_min_boundary_KP <- min(Key_Info_Typology_Model_Reach[[id_XR]]$RUGFile$KP_start)
-        RUG_max_boundary_KP <- max(Key_Info_Typology_Model_Reach[[id_XR]]$RUGFile$KP_end)
+    # Loop through Typology
+    for (id_typology in seq_along(SU_key_HM)) {
+        # Get KP boundaries of all SU of the each typology
+        SU_KP_boundaries_list <- SU_KP_boundaries_structure[[id_typology]]
+        # Get HM KP values of the typology
+        data_id_reach_HM <- Key_Info_Typology_Model_Reach[[id_typology]]$Model_Reach_nodes
+
+        if (min(data_id_reach_HM) != min(unlist(SU_KP_boundaries_list))) stop(paste0("Min value of KP in the model (", min(data_id_reach_HM), ") is not presented in the boundaries of the spatialisation of the typology: ", names(SU_key_HM)[id_typology], ". Please verify the input Kmin or Kflood Key SU_MR"))
+
+        if (max(data_id_reach_HM) != max(unlist(SU_KP_boundaries_list))) stop("Min value of KP in the model (", max(data_id_reach_HM), ") is not presented in the boundaries of the spatialisation of the typology: ", names(SU_key_HM)[id_typology], ". Please verify the input Kmin or Kflood Key SU_MR")
+
+        RUG_min_boundary_KP <- min(Key_Info_Typology_Model_Reach[[id_typology]]$RUGFile$KP_start)
+        RUG_max_boundary_KP <- max(Key_Info_Typology_Model_Reach[[id_typology]]$RUGFile$KP_end)
 
         # Check if KP boundaries of each SU respect the KP boundaries of XR
         if (min(unlist(SU_KP_boundaries_list)) != RUG_min_boundary_KP ||
@@ -60,31 +68,31 @@ SU_constructor <- function(SU_key_HM,
                 max(unlist(SU_KP_boundaries_list))
             )))
         }
-        data_KP <- Key_Info_Typology_Model_Reach[[id_XR]]$KP_grid
-        data_reaches <- Key_Info_Typology_Model_Reach[[id_XR]]$reach
+
+        data_KP <- Key_Info_Typology_Model_Reach[[id_typology]]$KP_grid
+        data_reaches <- Key_Info_Typology_Model_Reach[[id_typology]]$reach
 
         # Adding layer 3 : declare all SUs into the structure
-        SU[[id_XR]] <- vector("list", length(SU_KP_boundaries_list))
-        names(SU[[id_XR]]) <- names(SU_KP_boundaries_list)
+        SU[[id_typology]] <- vector("list", length(SU_KP_boundaries_list))
+        names(SU[[id_typology]]) <- names(SU_KP_boundaries_list)
         # Loop through each SU
         for (id_SU in seq_along(SU_KP_boundaries_list)) {
             # Adding layer 4: assign properties of each SU
             # Starting with KP and reach
             boundaries <- SU_KP_boundaries_list[[id_SU]]
 
-            if (Key_Info_Typology_Model_Reach[[id_XR]]$Logical_decreasing) {
-                # If logical_decreasing is TRUE, boundaries must be reordered to the calculation
-                boundaries <- sort(boundaries)
-                # Include end (right) and exclude start (left)
+            if (Key_Info_Typology_Model_Reach[[id_typology]]$Logical_decreasing) {
+                # If logical_decreasing is TRUE
+                # Include first value and exclude last if it is the last SU of the typology
                 if (id_SU != length(SU_KP_boundaries_list)) {
                     position_match <- which(
-                        data_KP > boundaries[1] &
-                            data_KP <= boundaries[2]
+                        data_KP <= boundaries[1] &
+                            data_KP > boundaries[2]
                     )
                 } else {
                     position_match <- which(
-                        data_KP >= boundaries[1] &
-                            data_KP <= boundaries[2]
+                        data_KP <= boundaries[1] &
+                            data_KP >= boundaries[2]
                     )
                 }
             } else {
@@ -102,48 +110,154 @@ SU_constructor <- function(SU_key_HM,
                 }
             }
 
-            SU[[id_XR]][[id_SU]]$KP <- data_KP[position_match]
-            SU[[id_XR]][[id_SU]]$reach <- data_reaches[position_match]
+            SU[[id_typology]][[id_SU]]$KP <- data_KP[position_match]
+            SU[[id_typology]][[id_SU]]$reach <- data_reaches[position_match]
+            SU[[id_typology]][[id_SU]]$id_reach_SU_boundaries <- SU_KP_boundaries_list[[id_SU]]
 
             # Add Z and priors
             if (identical(
-                SU_key_HM[[id_XR]][[id_SU]]$function_SU,
+                SU_key_HM[[id_typology]][[id_SU]]$function_SU,
                 getCovariate_piecewise
             )) {
-                if (!("shiftPoints" %in% names(SU_key_HM[[id_XR]][[id_SU]]))) {
+                if (!("shiftPoints" %in% names(SU_key_HM[[id_typology]][[id_SU]]))) {
                     return(stop(paste0(
-                        "To apply getCovariate_piecewise function, shiftPoints must be passed as argument. Please check the XR = ", names(SU_key_HM)[[id_XR]], ", SU = ", names(SU_key_HM[[id_XR]])[[id_SU]]
+                        "To apply getCovariate_piecewise function, shiftPoints must be passed as argument. Please check the XR = ", names(SU_key_HM)[[id_typology]], ", SU = ", names(SU_key_HM[[id_typology]])[[id_SU]]
                     )))
                 }
-                SU[[id_XR]][[id_SU]]$Z <- getCovariate_piecewise(
-                    shiftPoints = SU_key_HM[[id_XR]][[id_SU]]$shiftPoints,
-                    KP_grid = SU[[id_XR]][[id_SU]]$KP
+                SU[[id_typology]][[id_SU]]$Z <- getCovariate_piecewise(
+                    shiftPoints = SU_key_HM[[id_typology]][[id_SU]]$shiftPoints,
+                    KP_grid = SU[[id_typology]][[id_SU]]$KP
                 )
+
+                df <- data.frame(
+                    KP = SU[[id_typology]][[id_SU]]$KP,
+                    SU[[id_typology]][[id_SU]]$Z
+                )
+
+                df$pattern <- apply(df[, names(df) != "KP"], 1, paste0, collapse = "")
+
+                change_idx <- which(df$pattern != dplyr::lag(df$pattern))
+
+                if (length(change_idx) != 0) {
+                    KP_change <- df$KP[change_idx - 1]
+
+                    SU[[id_typology]][[id_SU]]$id_reach_SU_boundaries <- c(
+                        SU[[id_typology]][[id_SU]]$id_reach_SU_boundaries,
+                        KP_change
+                    )
+                }
             } else if (identical(
-                SU_key_HM[[id_XR]][[id_SU]]$function_SU,
+                SU_key_HM[[id_typology]][[id_SU]]$function_SU,
                 getCovariate_Legendre
             )) {
-                if (!("max_polynomial_degree" %in% names(SU_key_HM[[id_XR]][[id_SU]]))) {
+                if (!("max_polynomial_degree" %in% names(SU_key_HM[[id_typology]][[id_SU]]))) {
                     return(stop(paste0(
-                        "To apply getCovariate_Legendre function, max_polynomial_degree must be passed as argument. Please check the XR = ", names(SU_key_HM)[[id_XR]], ", SU = ", names(SU_key_HM[[id_XR]])[[id_SU]]
+                        "To apply getCovariate_Legendre function, max_polynomial_degree must be passed as argument. Please check the XR = ", names(SU_key_HM)[[id_typology]], ", SU = ", names(SU_key_HM[[id_typology]])[[id_SU]]
                     )))
                 }
-                SU[[id_XR]][[id_SU]]$Z <- getCovariate_Legendre(
-                    max_polynomial_degree = SU_key_HM[[id_XR]][[id_SU]]$max_polynomial_degree,
-                    covariate_discretization = SU[[id_XR]][[id_SU]]$KP
+                SU[[id_typology]][[id_SU]]$Z <- getCovariate_Legendre(
+                    max_polynomial_degree = SU_key_HM[[id_typology]][[id_SU]]$max_polynomial_degree,
+                    covariate_discretization = SU[[id_typology]][[id_SU]]$KP
                 )
             } else {
                 return(stop("function_SU given in input is not supported. Please select either getCovariate_Legendre or getCovariate_piecewise"))
             }
 
-            SU[[id_XR]][[id_SU]]$prior <- SU_key_HM[[id_XR]][[id_SU]]$prior
+            # Add all boundaries of reach_SU
+            SU[[id_typology]][[id_SU]]$id_reach_SU_boundaries <- sort(
+                SU[[id_typology]][[id_SU]]$id_reach_SU_boundaries,
+                decreasing = Key_Info_Typology_Model_Reach[[id_typology]]$Logical_decreasing
+            )
 
-            if (dim(SU[[id_XR]][[id_SU]]$Z)[2] != length(SU[[id_XR]][[id_SU]]$prior)) {
+            all_boundaries <- SU[[id_typology]][[id_SU]]$id_reach_SU_boundaries
+
+            # Add id_reach_SU
+            SU[[id_typology]][[id_SU]]$id_reach_SU <- sapply(
+                SU[[id_typology]][[id_SU]]$KP,
+                function(x) {
+                    bounds <- all_boundaries
+                    n <- length(bounds) - 1
+
+                    idx <- sapply(seq_len(n), function(i) {
+                        left <- bounds[i]
+                        right <- bounds[i + 1]
+                        # Decreasing
+                        if (Key_Info_Typology_Model_Reach[[id_typology]]$Logical_decreasing) {
+                            # Case 1: first interval → closed both sides
+                            if (i == 1) {
+                                cond <- x <= left & x >= right
+
+                                # Case 2: open left, closed right
+                            } else {
+                                cond <- x < left & x >= right
+                            }
+                        } else {
+                            # Case 1: first interval → closed both sides
+                            if (i == 1) {
+                                cond <- x >= left & x <= right
+
+                                # Case 2: open left, closed right
+                            } else {
+                                cond <- x > left & x <= right
+                            }
+                        }
+
+                        cond
+                    })
+
+                    idx <- which(idx)
+                    if (length(idx) == 0) {
+                        print(list(x = x, bounds = bounds))
+                        stop("No interval match for KP = ", x)
+                    }
+                    # Safety checks
+                    if (length(idx) == 0) {
+                        stop("No interval match for KP = ", x)
+                    } else if (length(idx) > 1) {
+                        stop("KP belongs to multiple intervals: ", x)
+                    } else {
+                        idx
+                    }
+                }
+            )
+
+            # Add priors
+            SU[[id_typology]][[id_SU]]$prior <- SU_key_HM[[id_typology]][[id_SU]]$prior
+
+            if (dim(SU[[id_typology]][[id_SU]]$Z)[2] != length(SU[[id_typology]][[id_SU]]$prior)) {
                 stop(paste0(
-                    "Error identified in XR = ", names(SU_key_HM)[[id_XR]], ", SU = ", names(SU_key_HM[[id_XR]])[[id_SU]], ". Number of columns of Z (", dim(SU[[id_XR]][[id_SU]]$Z)[2], ") must be equal to the length(prior) = ", length(SU[[id_XR]][[id_SU]]$prior)
+                    "Error identified in XR = ", names(SU_key_HM)[[id_typology]], ", SU = ", names(SU_key_HM[[id_typology]])[[id_SU]], ". Number of columns of Z (", dim(SU[[id_typology]][[id_SU]]$Z)[2], ") must be equal to the length(prior) = ", length(SU[[id_typology]][[id_SU]]$prior)
                 ))
             }
         }
     }
-    return(SU)
+
+    # Summary SU
+    summary_SU <- do.call(
+        rbind,
+        lapply(names(SU), function(section_name) {
+            section <- SU[[section_name]]
+
+            do.call(
+                rbind,
+                lapply(names(section), function(su_name) {
+                    SU_local <- section[[su_name]]
+                    id_vec <- SU_local$id_reach_SU_boundaries
+
+                    data.frame(
+                        typology = section_name,
+                        id_SU = su_name,
+                        id_reach_SU = seq_len(length(id_vec) - 1),
+                        KP_max_SU = id_vec[-length(id_vec)],
+                        KP_min_SU = id_vec[-1]
+                    )
+                })
+            )
+        })
+    )
+
+    return(list(
+        SU = SU,
+        summary_SU = summary_SU
+    ))
 }
