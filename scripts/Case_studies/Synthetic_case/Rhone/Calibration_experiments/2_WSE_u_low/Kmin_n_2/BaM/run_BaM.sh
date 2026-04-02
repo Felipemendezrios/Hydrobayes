ec102a5bb2fdff4009bad5c1f4dd6f7256dfa1d5
Felipemendezrios <famendezr@unal.edu.co>
2026-04-02 08:26:38 +0200

    Strop tracking folder 2_WSE_u_low

--- scripts/Case_studies/Synthetic_case/Rhone/Calibration_experiments/2_WSE_u_low/Kmin_n_2/BaM/run_BaM.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-'/home/famendezrios/Documents/Git/BaM/makefile/BaM' -cf '/home/famendezrios/Documents/These/VSCODE-R/HydroBayes/HydroBayes_git/scripts/Case_studies/Synthetic_case/Rhone/Calibration_experiments/2_WSE_u_low/Kmin_n_2/BaM/Config_BaM.txt'
