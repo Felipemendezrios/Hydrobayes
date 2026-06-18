b977b51b6f17443de2bb0b34191badd3f0b8c3fe
Felipemendezrios <famendezr@unal.edu.co>
2026-06-18 17:18:46 +0200

    Durance River case (calibration results)

---
+++ scripts/Case_studies/Durance_Thiercelin/Calibration_experiments/1_WSE_cte_u_0.05_without_rg_rd/Kmin_n_10/BaM/run_BaM.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+'/home/famendezrios/Documents/Git/BaM/makefile/BaM' -cf '/home/famendezrios/Documents/These/VSCODE-R/HydroBayes/HydroBayes_git/scripts/Case_studies/Durance_Thiercelin/Calibration_experiments/1_WSE_cte_u_0.05_without_rg_rd/Kmin_n_10_Q0/BaM/Config_BaM.txt'
