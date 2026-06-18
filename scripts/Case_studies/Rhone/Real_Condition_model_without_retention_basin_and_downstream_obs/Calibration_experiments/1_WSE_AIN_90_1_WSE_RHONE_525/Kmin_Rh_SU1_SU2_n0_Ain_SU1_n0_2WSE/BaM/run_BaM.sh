dd5c821d94fb749f198766dd93f2b5ccacf63d57
Felipemendezrios <famendezr@unal.edu.co>
2026-06-18 17:21:06 +0200

    Rhone case: without retention basin and without downstream observations in Ain tributary

---
+++ scripts/Case_studies/Rhone/Real_Condition_model_without_retention_basin_and_downstream_obs/Calibration_experiments/1_WSE_AIN_90_1_WSE_RHONE_525/Kmin_Rh_SU1_SU2_n0_Ain_SU1_n0_2WSE/BaM/run_BaM.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+'/home/famendezrios/Documents/Git/BaM/makefile/BaM' -cf '/home/famendezrios/Documents/These/VSCODE-R/HydroBayes/HydroBayes_git/scripts/Case_studies/Rhone/Real_Condition_model_without_retention_basin_and_downstream_obs/Calibration_experiments/1_WSE_AIN_90_1_WSE_RHONE_525/Kmin_Rh_SU1_SU2_n0_Ain_SU1_n0_2WSE/BaM/Config_BaM.txt'
