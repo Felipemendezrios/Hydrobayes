6f5847a625f1e1ffa01c82d1db22db3f9abf9ec7
Felipemendezrios <famendezr@unal.edu.co>
2026-06-18 17:29:52 +0200

    Rhone case: without retention basin and without downstream observations in Ain tributary and remove pseudo-obs

---
+++ scripts/Case_studies/Rhone/Real_Condition_model_without_retention_basin_and_downstream_obs_without_pseudo_obs/Calibration_experiments/Experiments_Input_Data/Kmin_Rh_SU1_SU2_n0_Ain_SU1_n0_2WSE.r
@@ -0,0 +1,288 @@
+############################################
+# Module 5 : BaM environment
+############################################
+
+# 1st layer: Parameters for vertical estimation of friction
+####################################################################
+
+# In MAGE context:
+# Constant piecewise function : Kmin and Kflood
+
+# In Dassflow context:
+# K = alpha * (H) ^ beta
+# Exponential function : so, alpha and beta are the parameters
+
+############################################
+# Kmin environment (encapsulated in Kmin_SU)
+############################################
+
+# Second layer: Set all SUs to each MR by Kmin or Kflood
+# Third layer: Set properties of each SU in terms of KP and spatialisation function
+####################################################################
+# Key to relate all SU by MR
+Input_Kmin_Key_SU_MR <- list(
+    # MR information
+    Rhone =
+        list(
+            SU1 = list(
+                # KP boundary points
+                KP_boundaries_points = c(55900, 34500),
+                # Function to apply at this SU
+                function_SU = getCovariate_Legendre,
+                # Arguments of this SU
+                max_polynomial_degree = 0,
+                prior = list(
+                    name_init = data.frame(
+                        name = c(
+                            "Km_SU1_Rh_a0"
+                        ),
+                        init = c(
+                            30
+                        )
+                    ),
+                    config = list(
+                        # Prior distribution: either Gaussian or FIX
+                        distribution = "Gaussian",
+                        # Specific spatially coordinates from the SU to apply the prior information. NULL indicates that algorithm will be used to spread as best as possible
+                        x_spatial = c(
+                            NULL
+                        ),
+                        # If only a values is given, all prior will be the same for all x_spatial.. If not, it must has the same size of x_spatial.
+                        param_values = data.frame(
+                            mu = c(
+                                30
+                            ),
+                            sigma = c(
+                                10
+                            )
+                        )
+                    )
+                )
+            ),
+            SU2 = list(
+                # KP boundary points
+                KP_boundaries_points = c(34500, 26750),
+                # Function to apply at this SU
+                function_SU = getCovariate_Legendre,
+                # Arguments of this SU
+                max_polynomial_degree = 0,
+                prior = list(
+                    name_init = data.frame(
+                        name = c(
+                            "Km_SU2_Rh_a0"
+                        ),
+                        init = c(
+                            30
+                        )
+                    ),
+                    config = list(
+                        # Prior distribution: either Gaussian or FIX
+                        distribution = "Gaussian",
+                        # Specific spatially coordinates from the SU to apply the prior information. NULL indicates that algorithm will be used to spread as best as possible
+                        x_spatial = c(
+                            NULL
+                        ),
+                        # If only a values is given, all prior will be the same for all x_spatial.. If not, it must has the same size of x_spatial.
+                        param_values = data.frame(
+                            mu = c(
+                                30
+                            ),
+                            sigma = c(
+                                10
+                            )
+                        )
+                    )
+                )
+            )
+        ),
+    # MR information
+    Ain =
+        list(
+            SU1 = list(
+                # KP boundary points
+                KP_boundaries_points = c(22333, 41461),
+                # Function to apply at this SU
+                function_SU = getCovariate_Legendre,
+                # Arguments of this SU
+                max_polynomial_degree = 0,
+                prior = list(
+                    name_init = data.frame(
+                        name = c(
+                            "Km_SU1_Ai_a0"
+                        ),
+                        init = c(19)
+                    ),
+                    config = list(
+                        # Prior distribution: either Gaussian or FIX
+                        distribution = "Gaussian",
+                        # Specific spatially coordinates from the SU to apply the prior information. NULL indicates that algorithm will be used to spread as best as possible
+                        x_spatial = c(
+                            NULL
+                        ),
+                        # If only a values is given, all prior will be the same for all x_spatial.. If not, it must has the same size of x_spatial.
+                        param_values = data.frame(
+                            mu = c(
+                                19
+                            ),
+                            sigma = c(
+                                8
+                            )
+                        )
+                    )
+                )
+            )
+        )
+)
+
+############################################
+# End Kmin environment
+############################################
+
+############################################
+# Kflood environment (encapsulated in Kflood_SU)
+############################################
+
+# Third layer: Set all SUs to each MR by Kflood
+# Fourth layer: Set properties of each SU in terms of KP and spatialisation function
+####################################################################
+# Key to relate all SU by MR
+Input_Kflood_Key_SU_MR <- list(
+    # MR information
+    Rhone =
+        list(
+            SU1 = list(
+                # KP boundary points
+                KP_boundaries_points = c(55900, 26750),
+                # Function to apply at this SU
+                function_SU = getCovariate_Legendre,
+                # Arguments of this SU
+                max_polynomial_degree = 0,
+                prior = list(
+                    name_init = data.frame(
+                        name = c(
+                            "Kf_SU1_Rh_a0"
+                        ),
+                        init = c(20)
+                    ),
+                    config = list(
+                        # Prior distribution: either Gaussian or FIX
+                        distribution = "FIX",
+                        # Specific spatially coordinates from the SU to apply the prior information. NULL indicates that algorithm will be used to spread as best as possible
+                        x_spatial = c(
+                            NULL
+                        ),
+                        # If only a values is given, all prior will be the same for all x_spatial.. If not, it must has the same size of x_spatial.
+                        param_values = data.frame(
+                            mu = c(
+                                20
+                            ),
+                            sigma = c(
+                                8
+                            )
+                        )
+                    )
+                )
+            )
+        ),
+    # MR information
+    Ain =
+        list(
+            SU1 = list(
+                # KP boundary points
+                KP_boundaries_points = c(22333, 41461),
+                # Function to apply at this SU
+                function_SU = getCovariate_Legendre,
+                # Arguments of this SU
+                max_polynomial_degree = 0,
+                prior = list(
+                    name_init = data.frame(
+                        name = c("Kf_SU1_Ai_a0"),
+                        init = c(15)
+                    ),
+                    config = list(
+                        # Prior distribution: either Gaussian or FIX
+                        distribution = "FIX",
+                        # Specific spatially coordinates from the SU to apply the prior information. NULL indicates that algorithm will be used to spread as best as possible
+                        x_spatial = c(
+                            NULL
+                        ),
+                        # If only a values is given, all prior will be the same for all x_spatial.. If not, it must has the same size of x_spatial.
+                        param_values = data.frame(
+                            mu = c(
+                                15
+                            ),
+                            sigma = c(
+                                7
+                            )
+                        )
+                    )
+                )
+            )
+        )
+)
+
+############################################
+# End Kflood environment
+############################################
+
+
+############################################
+# Qinflow (perturbations):
+# Perturbation could be different throught events
+# The order and the number of Q(t) does not change between events
+############################################
+
+# Fifth layer:
+# Set the prior information of mulitiplicative factor
+# be careful with the order of the event and the nodes
+# Prior associated to each dischage time series
+############################################
+
+# Default values :
+# mean = 1 -> no perturbation (centered to the value)
+# sd = 0.1 -> 10% of perturbation
+mult_factor <- list(
+    # Get order of the event from all_events defined in main file
+    event_1 = list(
+        # Get order of the nodes inside the .HYD file, must be sure that nodes positions are the same througth the events
+        node_1 = RBaM::parameter(
+            name = "Q_e1_PCH",
+            init = 1,
+            prior.dist = "FIX"
+        ),
+        node_2 = RBaM::parameter(
+            name = "Q_e1_LGN",
+            init = 1,
+            prior.dist = "FIX"
+        ),
+        node_3 = RBaM::parameter(
+            name = "Q_e1_BOU",
+            init = 1,
+            prior.dist = "FIX"
+        )
+    ),
+    event_2 = list(
+        node_1 = RBaM::parameter(
+            name = "Q_e2_PCH",
+            init = 1,
+            prior.dist = "FIX"
+        ),
+        node_2 = RBaM::parameter(
+            name = "Q_e2_LGN",
+            init = 1,
+            prior.dist = "FIX"
+        ),
+        node_3 = RBaM::parameter(
+            name = "Q_e2_BOU",
+            init = 1,
+            prior.dist = "FIX"
+        )
+    )
+)
+############################################
+# End Qinflow (perturbations)
+############################################
+
+############################################
+# End module 5: BaM environment
+############################################
