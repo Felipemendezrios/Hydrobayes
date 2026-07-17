c2cef5b08a930d5bd58db09d5b9724e45f20e059
Felipemendezrios <famendezr@unal.edu.co>
2026-07-17 16:28:55 +0200

    Remake model mage folder with relative paths Remove old folders Update gitignore

--- scripts/Case_studies/Rhone/SU1_Rh_bief_1_2_SU2_Rh_bief_3_SU1_Ai_bief_6_SU2_Ai_bief_4_5/Calibration_experiments/1_WSE_AIN_90_1_WSE_RHONE_525/Kmin_Rh_SU1_n4_SU2_n1_Ain_SU1_n4_SU2_n_0_2WSE/BaM/run_BaM.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-'/home/famendezrios/Documents/Git/BaM/makefile/BaM' -cf '/home/famendezrios/Documents/These/VSCODE-R/HydroBayes/HydroBayes_git/scripts/Case_studies/Rhone/SU1_Rh_bief_1_2_SU2_Rh_bief_3_SU1_Ai_bief_6_SU2_Ai_bief_4_5/Calibration_experiments/1_WSE_AIN_90_1_WSE_RHONE_525/Kmin_Rh_SU1_n4_SU2_n1_Ain_SU1_n4_SU2_n_0_2WSE/BaM/Config_BaM.txt'
