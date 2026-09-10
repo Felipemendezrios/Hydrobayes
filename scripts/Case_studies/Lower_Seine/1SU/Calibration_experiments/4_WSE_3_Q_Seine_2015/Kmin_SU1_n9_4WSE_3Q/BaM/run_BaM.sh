6eebc6c48d61e15218643ff3050905aebe4db42b
Felipemendezrios <famendezr@unal.edu.co>
2026-09-10 19:16:55 +0200

    Partial results of the Lower Seine River

---
+++ scripts/Case_studies/Lower_Seine/1SU/Calibration_experiments/4_WSE_3_Q_Seine_2015/Kmin_SU1_n9_4WSE_3Q/BaM/run_BaM.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+'/home/famendezrios/Documents/Git/BaM/makefile/BaM' -cf '/home/famendezrios/Documents/These/VSCODE-R/HydroBayes/HydroBayes_git/scripts/Case_studies/Lower_Seine/1SU/Calibration_experiments/4_WSE_3_Q_Seine_2015/Kmin_SU1_n9_4WSE_3Q/BaM/Config_BaM.txt'
